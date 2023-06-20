6381b5ec612f7bce5c1de40bcb3c7d895eb9a2cb
jonn <joliversalamanca@gmail.com>
2023-06-20 09:37:01 -0400

    new: MenuHead.css, MenunHead.jsx

--- src/components/MenuHead.jsx
+++ src/components/MenuHead.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import "./MenuHead.css"
 
 export function MenuHead(){
     return (
-        <nav>
-          <Link to="/sobre-mi">Sobre mi</Link> 
-          <Link to="/skills">Skills</Link> 
-          <Link to="/proyectos">Proyectos</Link> 
-          <Link to="/contactos">Contactos</Link> 
+        <nav className="menuHead-nav">
+          <NavLink className={({isActive}) => isActive? "menuHaead-navLink-00":null} to="/sobre-mi">Sobre mi</NavLink> 
+          <NavLink className={({isActive}) => isActive? "menuHaead-navLink-01":null} to="/skills">Skills</NavLink> 
+          <NavLink className={({isActive}) => isActive? "menuHaead-navLink-02":null} to="/proyectos">Proyectos</NavLink> 
+          <NavLink className={({isActive}) => isActive? "menuHaead-navLink-03":null} to="/contactos">Contactos</NavLink> 
         </nav>
     )
 }
